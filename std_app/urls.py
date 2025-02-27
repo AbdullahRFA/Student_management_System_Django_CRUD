028adcde7a1ba3eb8e41955a4de83bce2bfba269
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-27 19:03:09 +0600

    added base, about, index, services html file map them with peoject and their coresponding functions

--- std_app/urls.py
+++ std_app/urls.py
@@ -4,4 +4,6 @@ from std_app import views
 
 urlpatterns = [
     path("",views.HomePage,name='home'),
+    path("about/",views.AboutUS,name='about'),
+    path("services/",views.Services,name='services'),
 ]
--- std_app/views.py
+++ std_app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render,HttpResponse,redirect,get_object_or_404
 # Create your views here.
 def HomePage(request):
     return render(request,"std_app/index.html")
+def AboutUS(request):
+    return render(request,"std_app/about.html")
+def Services(request):
+    return render(request,"std_app/services.html")
+
